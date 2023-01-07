e7e6ce75f7bbe91062df4450de47c260f5cf6ab4
stormshark <53909021+stormshark@users.noreply.github.com>
2023-01-07 21:53:57 +0300

    adds fullscreen feature

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,34 +4,44 @@ const playButton  = document.querySelector('.player__button');
 const volumeChanger = document.querySelector("[name='volume']");
 const progressBarFilled = document.querySelector('.progress__filled');
 const progressBar = document.querySelector('.progress');
+const backForSec = document.querySelectorAll('.player__button');
+const fullScreenBtn= document.querySelector('.fullscreen');
+
 let isPlaying = false ;
+let isFullScreen= false;
+
+volumeChanger.addEventListener("input",(e)=> { videoItself.volume = (e.currentTarget.value)});
 
 
+videoItself.addEventListener('click', playOrPause);
 playButton.addEventListener("click",playOrPause);
  function playOrPause (e) {
     isPlaying = !isPlaying;
     isPlaying ? (videoItself.play() , playButton.textContent='❚ ❚')  : ( videoItself.pause(), playButton.textContent='►');
  }
 
- volumeChanger.addEventListener("input",(e)=> {
-    videoItself.volume = (e.currentTarget.value);
-});
 
- videoItself.addEventListener('click', playOrPause);
  videoItself.addEventListener('timeupdate', updateProgressBar)
-
-
  function updateProgressBar (e) {
     let currentVideoCompletionRatio = (videoItself.currentTime /videoItself.duration)*100;
     progressBarFilled.style.flexBasis = `${currentVideoCompletionRatio}%`;
  }
  
 progressBar.addEventListener("click",updateVideoTİme);
-
 function updateVideoTİme (e) {
     let timeLine =  (e.offsetX / progressBar.offsetWidth) * videoItself.duration
     videoItself.currentTime= timeLine
 }
 
 
+backForSec.forEach(item => item.addEventListener("click",changeTime))
+function changeTime(e) {
+    console.log("before update", videoItself.currentTime );
+    videoItself.currentTime += parseFloat(this.dataset.skip);
+    console.log("after update", videoItself.currentTime );
+}
 
+const playRate = document.querySelector("[name='playbackRate']");
+playRate.addEventListener("input", (e) =>  videoItself.playbackRate = e.currentTarget.value);
+
+fullScreenBtn.addEventListener("click", ()=>videoItself.requestFullscreen());
